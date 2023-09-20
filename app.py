95b624242dea32d8e58f2cc3917e8a7eacb54ea2
parthjuneja63 <parth639juneja@gmail.com>
2023-09-21 00:34:42 +0530

    Working Prototype

--- app.py
+++ app.py
@@ -14,12 +14,28 @@ def index():
     Reply = ""
     if request.method == "POST":
         names = request.form["names"]
+        gender = request.form["gender"]
         age = request.form["age"]
         query = request.form["query"]
+        noc = request.form["noc"]
+        rwc = request.form["rwc"]
+        doi = request.form["doi"]
+        toi = request.form["toi"]
+        place = request.form["poi"]
+        address = request.form["address"]
+        pn = request.form["pn"]
+        witnesses = request.form["witnesses"]
+        evidence = request.form["evidence"]
+        injuries = request.form["injuries"]
+        loss = request.form["loss"]
+        aoc = request.form["aoc"]
+        city = request.form["city"]
+        
+
         
         with open("data.txt", "w", encoding="utf-8") as f:
-            f.writelines("i want to file a case so create a draft application for me in totally legal format and also mention the IPC section and every analysis of the case, also mention the details i am giving to you, and don't mention anything about police station, here are the details:"+"\n")
-            f.writelines("my name:"+str(names)+"\n"+"my age:"+str(age)+"\n"+"what happened with me:"+str(query)+"\n"+"date and time on which it happened :"+str(datetime.date.today())+"\n")
+            f.writelines("I want to file a case so create a draft application for me in totally legal format and also mention the IPC section, also mention the details I am giving to you, and don't mention anything about police station and case number. Also don't include or mention anyone's signature in the application. Here are the details:"+"\n")
+            f.writelines("my name:"+str(names)+"\n"+"my gender:"+str(gender)+"\n"+"my age:"+str(age)+"\n"+"what happened with me:"+str(query)+"\n"+"name of culprit :"+"\n"+str(noc)+"\n"+"relation with culprit:"+str(rwc) + "address of culprit:" + str(aoc) + "\n" +"\n"+"date of incident:"+str(doi)+"\n"+"time of incident:"+str(toi)+"\n"+"place of incident:"+str(place)+"\n"+"my address"+str(address)+"\n" + "My city / district:" + str(city) + "\n" +"my mobile number"+str(pn)+"\n"+"my witnesses:"+str(witnesses)+"\n"+"evidence:"+str(evidence)+"\n"+"my injuries:"+str(injuries)+"\n"+"loss i have to face:"+str(loss)+"\n" )
         with open("data.txt", "r", encoding="utf-8") as f:
             R=f.read()
         R_variable=R
